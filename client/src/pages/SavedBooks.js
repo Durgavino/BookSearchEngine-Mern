2c0005773c47252fed7b9622d80bb41e47e1cfdc
Durgavino <mailtodurga93@gmail.com>
2023-04-05 17:47:53 -0300

    modified the save books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+//import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery,useMutation } from'@apollo/react-hooks';
+import { useQuery } from'@apollo/react-hooks';
 import {QUERY_ME} from '../utils/queries';
 //import { getMe, deleteBook } from '../utils/API';
 //import { getMe } from '../utils/queries';
@@ -11,9 +11,10 @@ import { removeBookId } from '../utils/localStorage';
 const SavedBooks = () => {
  // const [userData, setUserData] = useState({});
 const{ loading,data }=useQuery(QUERY_ME);
-const [remove_Book,{error}]=useMutation(removeBook);
+//const [remove_Book,{error}]=useMutation(removeBook);
 
-const userList=data?.me || [];
+const userList=data?.me || {} ;
+console.log(userList);
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -55,12 +56,13 @@ const userList=data?.me || [];
       </Jumbotron>
       <Container>
         <h2>
-          {userList.savedBooks.length
-            ? `Viewing ${userList.savedBooks.length} saved ${userList.savedBooks.length === 1 ? 'book' : 'books'}:`
+        {userList.savedBooks?.length
+            ? `Viewing ${userList.savedBooks.length} saved ${userList.savedBooks.length === 1 ? 'book' : 'books'
+            }:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userList.savedBooks.map((book) => {
+        {userList.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,7 +23,7 @@ const resolvers = {
         me: async (parent, args, context) => {
           if (context.user) {
             const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-    
+    console.log(userData,"ME QUERY");
             return userData;
           }
     
